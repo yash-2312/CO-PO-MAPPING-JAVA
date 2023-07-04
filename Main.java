04d0bb0891e8001fa553b97695693f0842ad7269
Yash Dayma <128605016+yash-2312@users.noreply.github.com>
2023-07-04 11:00:30 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+package test.BasicJava;
+
+public class Main{
+    public static void main(String[] args)
+    {
+//        System.out.println("hello world");  //here we are printing the hello world program to testing our compiler is rightly running or not.
+//        int a,b,sum;  //here we have started with the variables and its rules.
+//        a=34;
+//        b=56;
+//        sum=a+b;
+//        System.out.println(sum);
+//        int sum=0;
+//        for(int i=0;i<=78;i++)        //here we are applying the for loop for the printing of the sum of the first 78 natural no's.
+//            sum=sum+i;
+//        {
+//            System.out.println(sum);
+//        }
+//        int a=20,b=44;         //here I have taken the if and else conditional statement example in which according to the conditions it will give me the output
+//        if(b>a){
+//            System.out.println(true);
+//        }
+//        else{
+//            System.out.println(false);
+//        }
+//        Scanner sc=new Scanner(System.in);  //to take the inputs in java we firstly import the function then we make the use of the scanner function.
+//        int a= sc.nextInt(); //to take the input of the datatype int we use this method
+//        String name=sc.next();//to take the input of the datatype string we use this method.and it will give the output only single line
+
+         //take the two inputs from the user and print the sum of two no's
+//         Scanner sc=new Scanner(System.in);
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        int sum=a+b;
+//        System.out.println(sum);
+
+
+
+
+    }
+}
